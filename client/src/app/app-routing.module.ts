f076fb024907b3c9c1d3865e946be61ad3dbc3d9
natalypvmail@gmail.com <Morhuhn20o0>
2020-03-12 20:57:05 +0200

    login page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {LoginPageComponent} from "./login-page/login-page.component";
+import {AuthLayoutComponent} from "./layouts/auth-layout/auth-layout.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AuthLayoutComponent, children: [
+      {path: 'login', component: LoginPageComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    AuthLayoutComponent
   ],
   imports: [
     BrowserModule,
